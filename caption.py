df959e4c5c40b27b58ce4319bf93abfc815b6362
Larry Neal <nealla@lwneal.com>
2017-03-06 20:01:38 -0800

    Allow multiple input images for caption cli

--- caption.py
+++ caption.py
@@ -6,9 +6,9 @@ from util import predict, decode_jpg
 
 if __name__ == '__main__':
     model_filename = sys.argv[1]
-    img_filename = sys.argv[2]
-
-    pixels = decode_jpg(img_filename)
     model = models.load_model(model_filename)
-    os.system('imgcat ' + img_filename)
-    print(predict(model, pixels))
+
+    for img_filename in sys.argv[2:]:
+        pixels = decode_jpg(img_filename)
+        os.system('imgcat ' + img_filename)
+        print(predict(model, pixels))
