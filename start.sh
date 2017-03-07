51918f1ab09b3217bc9ff7de459af7c38ffa8eff
Larry Neal <nealla@lwneal.com>
2017-03-06 20:01:33 -0800

    Fix download script

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading the COCO dataset"
 mkdir $HOME/data
-./download_coco_dataset.sh $HOME/data
+./download_coco.sh $HOME/data
 python load_grefexp_to_redis.py $HOME/data
 python train.py model.h5
 python caption.py model.h5 cat.jpg
