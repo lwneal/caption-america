5711ec0c846da57079b388b3f42dabb073c18be4
Larry Neal <nealla@lwneal.com>
2017-03-08 14:22:42 -0800

    Remove fixed-size mean/flatten for resnet
    We want a variable-sized reduce at the end of the FCN

--- resnet50.py
+++ resnet50.py
@@ -186,9 +186,8 @@ def ResNet50(include_top=True, weights='imagenet',
     x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
     x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
 
-    x = AveragePooling2D((7, 7), name='avg_pool')(x)
-
     if include_top:
+        x = AveragePooling2D((7, 7), name='avg_pool')(x)
         x = Flatten()(x)
         x = Dense(1000, activation='softmax', name='fc1000')(x)
 
