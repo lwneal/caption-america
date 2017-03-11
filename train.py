22d2c27766071210e868dc8f3e5913ccdfa4264c
Lawrence Neal <nealla@oregonstate.edu>
2017-03-10 19:09:10 -0500

    Remove tensorboard callback

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import importlib
 # Each sentence contains ~10 words
 # One epoch should be around 500k, or ~100 iterations
 # We want to train for ten or twenty epochs
-iter_count = 4000
+iter_count = 40000
 
 module_name = sys.argv[1]
 module_name = module_name.rstrip('.py')
@@ -21,7 +21,7 @@ model = target.build_model()
 if os.path.exists(model_filename):
     model.load_weights(model_filename)
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'], decay=.01, lr=.001)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'], decay=.003, lr=.001)
 
 g = target.training_generator()
 
@@ -29,7 +29,5 @@ for i in range(iter_count):
     samples = 2**12
     print("Trained {}k samples:".format(i * samples / 2**10))
     target.demo(model)
-    tb_callback = keras.callbacks.TensorBoard(log_dir='/home/nealla/tensorboard', 
-            histogram_freq=0, write_graph=True, write_images=True)
-    model.fit_generator(g, samples_per_epoch=samples, nb_epoch=1, callbacks=[tb_callback])
+    model.fit_generator(g, samples_per_epoch=samples, nb_epoch=1)
     model.save_weights(model_filename)
