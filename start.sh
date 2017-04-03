4e1d374c5221e46a145bbb458ae5b6bd64fe76d0
Larry Neal <nealla@lwneal.com>
2017-04-03 12:45:12 -0700

    Update ./start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,4 @@ mkdir $HOME/data
 ./download_coco.sh $HOME/data
 python load_grefexp_to_redis.py $HOME/data
 python load_coco_to_redis.py $HOME/data
-python train.py model.h5
-python caption.py model.h5 cat.jpg
+python train.py caption.py
