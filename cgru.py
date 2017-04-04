4b919e3c2322f8f690f4e505ef4bcd41a097b076
Larry Neal <nealla@lwneal.com>
2017-04-04 09:03:53 -0700

    Update spatial recurrent demo

---
+++ cgru.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+import keras
+from keras import activations
+from keras import initializers
+from keras.layers import Recurrent
+from keras import backend as K
+from keras.utils import conv_utils
+from keras import activations
+
+
+class SpatialCGRU(Recurrent):
+    """ A 1D convolutional tanh/sigmoid GRU with no dropout and no regularization
+    Convolves forward along the first non-batch axis (ie from top to bottom of an image)
+    """
+    def __init__(self, units=10, *args, **kwargs):
+        # __init__ just sets params, doesn't allocate anything
+        super(SpatialCGRU, self).__init__(**kwargs)
+        self.units = units
+
+        # TODO: Handle all the normal RNN parameters
+        self.activation = activations.get('tanh')
+        self.recurrent_activation = activations.get('hard_sigmoid')
+        self.dropout = 0
+        self.recurrent_dropout = 0
+
+        # TODO: Handle all the normal Conv1D parameters
+        self.filter_size = 3
+        self.padding = conv_utils.normalize_padding('same')
+        # Keras hard-codes the RNN ndim to 3; let's change it to 4
+        from keras.engine.topology import InputSpec
+        self.input_spec = InputSpec(ndim=4)
+
+
+    def build(self, input_shape):
+        print("Calling build() for ConvGRU with input shape {}".format(input_shape))
+        batch_size = input_shape[0]
+        time_steps = input_shape[1]
+        pixels_per_step = input_shape[2]
+        channels_per_pixel = input_shape[3]
+
+        # Notice that the input_spec changes after __init__ and again after build(), becoming more restrictive
+        self.input_spec = keras.engine.InputSpec(shape=(batch_size, None, pixels_per_step, channels_per_pixel))
+        self.state_spec = keras.engine.InputSpec(shape=(batch_size, pixels_per_step, self.units))
+
+        # TODO: does Keras require self.states?
+        self.states = [None]
+
+        self.Wz = self.add_weight((self.filter_size, self.units, self.units),
+                name='Wz', initializer=initializers.get('glorot_uniform'))
+        self.Wr = self.add_weight((self.filter_size, self.units, self.units),
+                name='Wr', initializer=initializers.get('glorot_uniform'))
+        self.Wh = self.add_weight((self.filter_size, self.units, self.units),
+                name='Wh', initializer=initializers.get('glorot_uniform'))
+        self.Uz = self.add_weight((self.filter_size, channels_per_pixel, self.units),
+                name='Uz', initializer=initializers.get('glorot_uniform'))
+        self.Ur = self.add_weight((self.filter_size, channels_per_pixel, self.units),
+                name='Ur', initializer=initializers.get('glorot_uniform'))
+        self.Uh = self.add_weight((self.filter_size, channels_per_pixel, self.units),
+                name='Uh', initializer=initializers.get('glorot_uniform'))
+        self.built = True
+
+    """ The step() method is a magical function which is passed into tf.while_loop()
+        It must have the following special properties:
+        Parameters:
+            input: tensor with shape `(samples, ...)` (no time dimension),
+                representing input for the batch of samples at a certain
+                time step.
+            states: list of tensors.
+        Returns:
+            output: tensor with shape `(samples, output_dim)`
+                (no time dimension).
+            new_states: list of tensors, same length and shapes
+                as 'states'. The first state in the list must be the
+                output tensor at the previous timestep.
+    """
+    def step(self, input_tensor, state_tensors):
+        # 1 x self.units
+        prev_y = state_tensors[0]
+        # 1 x self.input_dim
+        x_t = input_tensor
+
+
+        z = self.recurrent_activation(tf.nn.convolution(x_t, self.Uz, padding='SAME') + tf.nn.convolution(prev_y, self.Wz, padding='SAME'))
+        r = self.recurrent_activation(tf.nn.convolution(x_t, self.Ur, padding='SAME') + tf.nn.convolution(prev_y, self.Wr, padding='SAME'))
+        h = self.activation(
+                tf.nn.convolution(x_t, self.Uh, padding='SAME') +
+                tf.nn.convolution(prev_y * r, self.Wh, padding='SAME')
+            )
+        y_t = (1 - z) * h + z * prev_y
+        return y_t, [y_t]
+
+    """
+    Keras will call this function when you first build the layer.
+    """
+    def get_initial_states(self, inputs):
+        # Our default spatial CGRU will scan the image top to bottom
+        #   TODO: Learn to implement a Rule 110 cellular automaton as a demonstration
+        # Input shape is BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, CHANNELS
+        # Input at each time step is BATCH_SIZE, IMG_WIDTH, CHANNELS
+        # Output at each time step is IMG_WIDTH, UNITS 
+        # For GRUs, there is no other state but output
+        shape = inputs.shape.as_list()
+        batch_size, time_steps, pixels_per_step, channels_per_pixel = shape
+        initial_state = K.zeros((batch_size, pixels_per_step, self.units))
+        return [initial_state]
+
+    def compute_output_shape(self, input_shape):
+        batch_size, time_steps, pixels_per_step, channels_per_pixel = input_shape
+        return (batch_size, time_steps, pixels_per_step, self.units)
+
--- spatial_recurrent.py
+++ spatial_recurrent.py
@@ -1,70 +1,131 @@
 import numpy as np
-import tensorflow as tf
 from keras import layers, models
-from keras.layers import TimeDistributed as TD
+from PIL import Image
+import tensorflow as tf
+from keras import backend as K
 
-BATCH_SIZE = 1
-IMG_WIDTH = 11
-IMG_HEIGHT = IMG_WIDTH
-GRU_SIZE = 20
+import imutil
+from cgru import SpatialCGRU
 
-print("Keras won't let you change the batch size, so set it to 1")
-img = layers.Input(batch_shape=(1, BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, 3))
-
-x = TD(layers.Convolution2D(1,1,1))(img)
+print("Setting arrays to pretty-print")
+np.set_printoptions(formatter={'float_kind':lambda x: "% .1f" % x})
 
 
-print("Transforming space into time...")
-rows = layers.Lambda(lambda x: tf.reshape(x, [1, -1, IMG_WIDTH, 1]))(x)
+cat = np.array(Image.open('kitten.jpg').resize((32,32)))
+dog = np.array(Image.open('puppy.jpg').resize((32,32)))
+def example():
+    pixels = np.zeros((224, 224, 3))
+    rand = lambda: np.random.randint(1, 224-32-1)
+    cx, cy = rand(), rand()
+    pixels[cy:cy+32, cx:cx+32] = cat
+    dx, dy = rand(), rand()
+    pixels[dy:dy+32, dx:dx+32] = dog
 
-t = layers.Lambda(lambda x: tf.transpose(x, [0, 1, 3, 2, 4]))(x)
-cols = layers.Lambda(lambda x: tf.reshape(x, [1, -1, IMG_HEIGHT, 1]))(t)
-
-groo = TD(layers.GRU(GRU_SIZE, return_sequences=True))
-backwards = layers.Lambda(lambda x: tf.reverse(x, axis=[1]))
-left_rnn = groo(rows)
-down_rnn = groo(cols)
-right_rnn = groo(backwards(rows))
-up_rnn = groo(backwards(cols))
-
-reshape_to_mask = layers.Lambda(lambda x: tf.reshape(x, [1, BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, GRU_SIZE]))
-
-left_out = reshape_to_mask(left_rnn)
-right_out = reshape_to_mask(backwards(right_rnn))
-
-down_out = layers.Lambda(lambda x: tf.transpose(x, [0, 2, 1, 3]))(down_rnn)
-down_out = reshape_to_mask(down_out)
-
-up_out = layers.Lambda(lambda x: tf.transpose(x, [0, 2, 1, 3]))(up_rnn)
-up_out = reshape_to_mask(backwards(up_out))
-
-concat_out = layers.merge([left_out, right_out, up_out, down_out], mode='concat', concat_axis=-1)
-
-print("... now transforming time back into space")
-output_mask = TD(layers.Convolution2D(1,1,1))(concat_out)
-
-moo = models.Model(input=img, output=output_mask)
-rando = np.random.rand(BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH,3)
-rando = np.expand_dims(rando, axis=0)
-preds = moo.predict(rando)
-print("Got output preds shape {}".format(preds.shape))
+    # Target: Light up for all pixels that are ABOVE the cat AND RIGHT OF the dog
+    target = up_cone(cx, cy) + right_cone(dx, dy)
+    target = (target > 1).astype(np.float)
+    return pixels, target
 
 
+def up_cone(x, y, input_shape=(224,224), scale=7/224.):
+    height, width = int(input_shape[0]*scale), int(input_shape[1]*scale)
+    Y = np.zeros((height, width, 1))
+    pos_y, pos_x = int(y * scale), int(x * scale)
+    for i in range(pos_y, -1, -1):
+        left = pos_x - (pos_y - i)
+        right = pos_x + (pos_y - i) + 1
+        left = max(0, left)
+        Y[i, left:right, 0] = 1.0
+    return Y
+    
 
-X = np.zeros((1, 1, 11, 11, 3))
-Y = np.zeros((1, 1, 11, 11, 1))
+def right_cone(x, y, input_shape=(224,224), scale=7/224.):
+    height, width = int(input_shape[0]*scale), int(input_shape[1]*scale)
+    Y = np.zeros((height, width, 1))
+    pos_y, pos_x = int(y * scale), int(x * scale)
+    for i in range(pos_x, width):
+        bot = pos_y - (i - pos_x)
+        top = pos_y + (i - pos_x) + 1
+        bot = max(0, bot)
+        Y[bot:top, i, 0] = 1.0
+    return Y
 
-X[:,:,1,0,:] = 1.0
-Y[:,:,:,0,:] = 1.0
-Y[:,:,1,:,:] = 1.0
 
-print("Input X:")
-print(X[0,0,:,:,0])
+def map_to_img(Y, scale=224./7):
+    output = np.zeros((224,224,3))
+    from scipy.misc import imresize
+    output[:,:,0] = imresize(Y[:,:,0], scale)
+    output *= Y.max()
+    return output
 
-print("Target Y:")
-print(Y[0,0,:,:,0])
 
-moo.compile(optimizer='adam', loss='mae')
-moo.train_on_batch(X,Y)
-moo.fit(X, Y, nb_epoch=1000)
-preds = moo.predict(X)
+def build_model():
+    BATCH_SIZE = 1
+    IMG_WIDTH = 224
+    IMG_HEIGHT = IMG_WIDTH
+    IMG_CHANNELS = 3
+
+    CRN_INPUT_SIZE = 8
+    CRN_OUTPUT_SIZE = 5
+
+    img = layers.Input(batch_shape=(BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+
+    # Apply the convolutional layers of VGG16
+    from keras.applications.vgg16 import VGG16
+    vgg = VGG16(include_top=False)
+    for layer in vgg.layers:
+        layer.trainable = False
+
+    # Convolve the image
+    x = vgg(img)
+    x = layers.Conv2D(CRN_INPUT_SIZE, (1,1))(x)
+
+    # Statefully scan the image in each of four directions
+    cgru = SpatialCGRU(CRN_OUTPUT_SIZE, return_sequences=True)
+
+    r = layers.Lambda(lambda x: K.reverse(x, 1))
+    t = layers.Lambda(lambda x: tf.transpose(x, [0, 2, 1, 3]))
+
+    down_rnn = cgru(x)
+    up_rnn = r(cgru(r(x)))
+    left_rnn = cgru(t(x))
+    right_rnn = r(cgru(r(t(x))))
+
+    concat_out = layers.merge([left_rnn, right_rnn, up_rnn, down_rnn], mode='concat', concat_axis=-1)
+
+    # Convolve the image some more
+    output_mask = layers.Conv2D(1, (1,1), activation='sigmoid')(concat_out)
+
+    moo = models.Model(inputs=img, outputs=output_mask)
+    moo.compile(optimizer='adam', loss='mse')
+    return moo
+
+
+def train(model):
+    X, Y = example()
+    print("Input X:")
+    imutil.show(X)
+
+    print("Target Y:")
+    imutil.show(map_to_img(Y))
+
+    while True:
+        for i in range(100):
+            X, Y = example()
+            X = np.expand_dims(X, axis=0)
+            Y = np.expand_dims(Y, axis=0)
+            h = model.train_on_batch(X, Y)
+
+        preds = model.predict(X)
+        print("Input:")
+        imutil.show(X)
+        print("Network Output:")
+        imutil.show(map_to_img(preds[0]))
+        print("Min {:.2f} Max {:.2f}".format(preds.min(), preds.max()))
+        print("Ground Truth:")
+        imutil.show(map_to_img(Y[0]))
+
+
+if __name__ == '__main__':
+    model = build_model()
+    train(model)
